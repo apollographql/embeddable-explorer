cb37f6fce5413647ce7fb9431d8f21865d5c609a
Chang Wang <cheapsteak@gmail.com>
2022-03-01 12:33:44 -0500

    cherry picked react example

---
+++ src/react/index.tsx
@@ -0,0 +1,22 @@
+import React, { useEffect } from 'react';
+import EmbeddedExplorer, {
+  EmbeddableExplorerOptions,
+} from '../EmbeddedExplorer'
+
+const DEFAULT_TARGET = 'apollo-explorer';
+
+export const ApolloExplorerReact = (
+  props: Omit<EmbeddableExplorerOptions, 'target'> & {
+    className?: string;
+    target?: string;
+  }
+) => {
+  useEffect(() => {
+    new EmbeddedExplorer({ ...props, target: props.target ?? DEFAULT_TARGET });
+  }, []);
+
+  return (
+    <div className={props.className} id={props.target ?? DEFAULT_TARGET} />
+  );
+};
+
