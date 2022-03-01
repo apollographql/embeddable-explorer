7fe3fc02a147f4d2b6fb8fbcb82aac244b3028cc
Chang Wang <cheapsteak@gmail.com>
2022-03-01 12:56:56 -0500

    only setup default export for umd build

---
+++ tsdx.config.js
@@ -0,0 +1,9 @@
+module.exports = {
+  rollup(config, options) {
+    if (config.output.format === 'umd') {
+        config.output.name = 'EmbeddedExplorer';
+        config.output.exports = 'default';
+    }
+    return config;
+  },
+};
