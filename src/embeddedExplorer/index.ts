ab251bbab4fe54e5809017e36908db051ce27863
mayakoneval <mayakoneval@gmail.com>
2022-04-29 12:08:58 -0700

    don't export ApolloSandbox

--- src/embeddedExplorer/index.ts
+++ src/embeddedExplorer/index.ts
@@ -1,6 +1,4 @@
 import { EmbeddedExplorer } from './EmbeddedExplorer';
-import { EmbeddedSandbox } from '../embeddedSandbox/EmbeddedSandbox';
 import { ApolloExplorerReact } from './react';
 
 export { EmbeddedExplorer as ApolloExplorer, ApolloExplorerReact };
-export { EmbeddedSandbox as ApolloSandbox };
