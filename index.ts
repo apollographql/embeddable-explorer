e485f3eaf2b06fb923aadb58f8ab19e920759648
mayakoneval <mayakoneval@gmail.com>
2022-05-17 16:12:09 -0700

    move index to actual correc tplace

--- index.ts
+++
@@ -1,4 +0,0 @@
-import { EmbeddedExplorer } from './src/embeddedExplorer/EmbeddedExplorer';
-import { ApolloExplorerReact } from './src/embeddedExplorer/react';
-
-export { EmbeddedExplorer as ApolloExplorer, ApolloExplorerReact };
