26c885f4da746e9451076d10bbbb6557d108c10d
mayakoneval <mayakoneval@gmail.com>
2022-06-09 13:36:51 -0400

    build cjs & esm with rollup. The build is going to look a litle different than before re: naming, but it achieves the same thing

--- buildHelpers/createRollupConfig.js
+++ buildHelpers/createRollupConfig.js
@@ -1,5 +1,6 @@
 import typescript from '@rollup/plugin-typescript';
 import { terser } from 'rollup-plugin-terser';
+import rename from 'rollup-plugin-rename';
 
 // type RollupOptions = {
 //   format: 'umd' | 'cjs' | 'esm',
@@ -40,6 +41,58 @@ export function createUMDRollupConfig(options) {
           ecma: 2020,
           module: false,
           toplevel: false,
+        }),
+    ],
+  };
+}
+
+export function createCJS_ESMRollupConfig(options) {
+  return {
+    input: 'src/index.ts',
+    output: {
+      format: options.format,
+      freeze: false,
+      esModule: true,
+      name: options.isExplorer ? 'embeddable-explorer' : 'embeddable-sandbox',
+      exports: 'named',
+      sourcemap: true,
+      dir: `./dist`,
+      entryFileNames:
+        options.format === 'esm'
+          ? '[name].mjs'
+          : options.environment === 'production'
+          ? '[name].production.min.js'
+          : '[name].development.js',
+      ...(options.format === 'cjs' && {
+        chunkFileNames:
+          options.environment === 'production'
+            ? '[name].production.min.js'
+            : '[name].development.js',
+      }),
+    },
+    external: ['use-deep-compare-effect', 'react'],
+    plugins: [
+      typescript({
+        tsconfig: './tsconfig.json',
+      }),
+      rename({
+        map: (name) =>
+          name
+            .replace('src/', '')
+            .replace('node_modules/', 'external/')
+            .replace('../../external', '../external'),
+      }),
+      options.environment === 'production' &&
+        terser({
+          output: { comments: false },
+          compress: {
+            keep_infinity: true,
+            pure_getters: true,
+            passes: 10,
+          },
+          ecma: 2020,
+          module: options.format === 'esm',
+          toplevel: options.format === 'esm' || options.format === 'cjs',
         }),
     ],
   };
---
+++ buildHelpers/writeCjsEntryFile.js
@@ -0,0 +1,13 @@
+const fs = require('fs-extra');
+
+const name = process.argv[2];
+const baseLine = `module.exports = require('./${name}`;
+const contents = `
+  'use strict'
+  if (process.env.NODE_ENV === 'production') {
+    ${baseLine}.production.js')
+  } else {
+    ${baseLine}.development.js')
+  }
+  `;
+fs.outputFile('dist/index.cjs', contents);
