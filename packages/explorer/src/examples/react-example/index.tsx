b53579ac89d38cc44ec2529c55aa45133d600b1c
mayakoneval <mayakoneval@gmail.com>
2022-06-30 09:50:22 -0700

    remove unnecessary endpoint url in examples

--- packages/explorer/src/examples/react-example/index.tsx
+++ packages/explorer/src/examples/react-example/index.tsx
@@ -23,7 +23,6 @@ function App() {
       <ApolloExplorer
         className="embedded-explorer"
         graphRef="acephei@current"
-        endpointUrl="https://acephei-gateway.herokuapp.com"
         initialState={{
           document: `query Example {
 	me {
