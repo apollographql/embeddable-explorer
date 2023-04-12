0ae4f8d2ae324729d2bd1736d3afc800acf96b82
Maya Koneval <mayakoneval@gmail.com>
2023-04-11 17:03:39 -0700

    use stricter Explorer types & add in federation types for query plan stuff

--- packages/explorer-helpers/src/types.ts
+++ packages/explorer-helpers/src/types.ts
@@ -1,13 +1,46 @@
 import type { GraphQLError } from 'graphql';
-import type { ObjMap } from 'graphql/jsutils/ObjMap';
 
 export type JSONPrimitive = boolean | null | string | number;
 export type JSONObject = { [key in string]?: JSONValue };
 export type JSONValue = JSONPrimitive | JSONValue[] | JSONObject;
 const TRACE_KEY = 'ftv1';
+export const QUERY_PLAN_KEY = 'apolloQueryPlan';
+
+type PlanNode =
+  | {
+      kind: 'Defer';
+      deferred: {
+        node: PlanNode | null | undefined;
+        label: string | null | undefined;
+      }[];
+      primary: { node: PlanNode | null | undefined };
+    }
+  | { kind: 'Sequence'; nodes: PlanNode[]; operation: string }
+  | { kind: 'Parallel'; nodes: PlanNode[] }
+  | { kind: 'Fetch'; serviceName: string; operation?: string }
+  | {
+      kind: 'Flatten';
+      path: (string | number)[];
+      node: PlanNode;
+      operation?: string;
+    };
+
+export interface QueryPlan {
+  node: PlanNode;
+}
+
+export interface QueryPlanResponse {
+  text: string;
+  object: QueryPlan;
+}
+export interface Extensions {
+  [TRACE_KEY]?: string;
+  [QUERY_PLAN_KEY]?: QueryPlanResponse;
+}
+
 export interface ResponseData {
-  data?: Record<string, unknown> | JSONValue | ObjMap<unknown>;
+  data: Record<string, unknown> | JSONValue | undefined;
   path?: Array<string | number>;
-  errors?: readonly GraphQLError[];
-  extensions?: { [TRACE_KEY]?: string };
+  errors?: Array<GraphQLError>;
+  extensions?: Extensions;
 }
